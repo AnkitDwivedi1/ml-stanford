1a4715dd5e6cd2271950d4d8d39d77da15481e17
dagget20 <juampalg85@gmail.com>
2015-06-25 01:04:30 -0500

    working cost

--- computeCostMulti.m
+++ computeCostMulti.m
@@ -13,7 +13,10 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
+predictions = X * theta;
+error = (predictions - y);
 
+J = 1/(2*m) * (error' * error);
 
 
 
